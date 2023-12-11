4ed947897f56847e2357ab84dea7a9ba02d9162f
Louis Stafford <louiststafford@gmail.com>
2023-12-11 18:39:07 +0000

    In progress edit button

--- express_server.js
+++ express_server.js
@@ -35,9 +35,10 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  urlDatabase[generateRandomString()] = req.body['longURL'];
+  const id = generateRandomString()
+  urlDatabase[id] = req.body['longURL'];
   // res.send("Ok"); // Respond with 'Ok' (we will replace this)
-  res.redirect("/urls/:id"); // Redirect to the /urls page
+  res.redirect(`/urls/${id}`); // Redirect to the /urls:id page
 });
 
 app.get("/urls/new", (req, res) => {
@@ -63,6 +64,12 @@ app.post("/urls/:id/delete", (req, res) => {
   } else {
     res.status(404).send("URL not found");
   }
+});
+
+app.post("/urls/:id/update", (req, res) => {
+  urlDatabase[req.params.id] = req.params.longURL;
+  console.log(`TEST 1: ${req.params.id}`);
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {

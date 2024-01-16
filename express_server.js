09326080f2b58e48c56a25da052a77929b239522
Louis Stafford <louiststafford@gmail.com>
2024-01-16 16:15:18 +0000

    Security improvement, cookie clearing update

--- express_server.js
+++ express_server.js
@@ -3,8 +3,9 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 8080;
+// these are all required, so don't get rid of them.
 
-const generateRandomString = function() {
+const generateRandomString = function() { // Generates a random string. Used to generate userIDs and shortened urls.
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let randomString = '';
   const length = 6;
@@ -15,7 +16,7 @@ const generateRandomString = function() {
   return randomString;
 };
 
-const users = {
+const users = { // the database for users. 
   userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
@@ -37,7 +38,7 @@ const searchThroughUsers = function(input, userItem) {
   return false;
 };
 
-const urlDatabase = {
+const urlDatabase = { // the database for all urls. DO NOT REMOVE.
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
     userID: "aJ48lW",
@@ -56,13 +57,13 @@ const urlsForUser = function(id) {
     }
   }
   return userUrls;
-};
+}; // a function that grabs the urls only the user inputted made. Searches through the larger data
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({
   name: 'session',
   keys: [
-    'secretKey'
+    'secretKey' // cookieSession breaks if this isn't here, so don't remove it.
   ],
 }));
 app.use((req, res, next) => {
@@ -71,40 +72,47 @@ app.use((req, res, next) => {
 });
 app.set("view engine", "ejs");
 
+
+
 app.get("/", (req, res) => {
   if (req.session.user_id) {
     return res.redirect(`/urls/`);
   }
   res.redirect('/login/');
+  // a simple redirect for the baseline site. 
 });
 
+// Below section up for deletion
+
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  res.json(urlDatabase); // keeping this around seems like a security breach, but I never saw any prompt to remove it. I'll let a mentor handle it.
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send("<html><body>Hello <b>World</b></body></html>\n"); // basically pointless. It's test code.
 });
+
+// Above section up for deletion
 
 app.get("/urls", (req, res) => {
   if (!req.session.user_id) {
     return res.send("<html><body>You cannot view URLs on this site if you are not logged in!</body></html>\n");
   }
-  const templateVars = {
-    urls: urlDatabase,
+  const templateVars = { // You'll see templateVars a lot. They're essentially variables that need to be sent to the ejs code for that to function.
+    urls: urlsForUser(req.session.user_id),
     user: users[req.session.user_id]
   };
-  res.render("urls_index", templateVars);
+  res.render("urls_index", templateVars); // including templateVars in the render lets it be sent to the page in views. Wahoo!
 });
 
-app.post("/urls", (req, res) => {
+app.post("/urls", (req, res) => { //this is what lets you make a shortened url. 
   if (!req.session.user_id) {
     return res.send("<html><body>You cannot shorten URLs on this site if you are not logged in!</body></html>\n");
   }
   console.log(req.body); // Log the POST request body to the console
   const id = generateRandomString();
-  urlDatabase[id] = {};
-  urlDatabase[id]['longURL'] = req.body.longURL;
+  urlDatabase[id] = {}; // makes the initial object for the id (so the below two lines don't return undefined and break something.)
+  urlDatabase[id]['longURL'] = req.body.longURL; 
   urlDatabase[id]['userID'] = req.session.user_id;
   res.redirect(`/urls/${id}`); // Redirect to the /urls:id page
 });
@@ -117,6 +125,7 @@ app.get("/urls/new", (req, res) => {
     user: users[req.session.user_id]
   };
   res.render("urls_new", templateVars);
+  // a simple render of a page. No fuss here.
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -128,7 +137,7 @@ app.get("/urls/:id", (req, res) => {
   }
   let idValid = false;
   let userUrls = urlsForUser(req.session.user_id);
-  for (let url in userUrls) {
+  for (let url in userUrls) { // this basically looks for the inputted id in urls the user's made. Since we're only worried about one url, a boolean check works here.
     if (req.params.id === url) {
       idValid = true;
       break;
@@ -137,6 +146,7 @@ app.get("/urls/:id", (req, res) => {
   if (idValid === false) {
     return res.send("<html><body>You are not the owner of this url!</body></html>");
   }
+  // all of the above in this section are security measures and crash prevention. 
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id].longURL,
@@ -150,6 +160,7 @@ app.get("/u/:id", (req, res) => {
     return res.send("<html><body>This id does not exist!</body></html>\n");
   }
   return res.redirect(urlDatabase[req.params.id]['longURL']);
+  // this just links you to your shortened url. 
 });
 
 app.get("/register", (req, res) => {
@@ -168,16 +179,17 @@ app.post("/register", (req, res) => {
       return res.status(400).send("Email is already registered.");
     }
   }
+  // The above code is security measures. No crashing the site with already used emails or lacking required inputs.
   const id = generateRandomString();
   const email = req.body.email;
   const password = req.body.password;
-  const hashedPassword = bcrypt.hashSync(password, 10);
-  users[id] = {
+  const hashedPassword = bcrypt.hashSync(password, 10); // encrypts your password. Safety first!
+  users[id] = { // puts the above consts (minus password, for safety) into the users database.
     id,
     email,
     password: hashedPassword,
   };
-  req.session.user_id = id;
+  req.session.user_id = id; // sets your id as a cookie.
   res.redirect("/urls");
 });
 
@@ -198,18 +210,19 @@ app.post("/urls/:id/delete", (req, res) => {
   if (idValid === false) {
     return res.send("<html><body>You are not the owner of this url!</body></html>");
   }
+  // all of the above are security measures.
   const idToDelete = req.params.id;
 
   if (urlDatabase[idToDelete]) {
     delete urlDatabase[idToDelete];
     return res.redirect("/urls");
   } else {
-    return res.status(404).send("URL not found");
+    return res.status(404).send("URL not found"); // this is here to prevent the site from crashing from getting something that doesn't exist.
   }
 });
 
 app.post("/urls/:id/update", (req, res) => {
-  urlDatabase[req.params.id].longURL = req.body.longURL;
+  urlDatabase[req.params.id].longURL = req.body.longURL;   // changes the chosen url to the new one submitted by the user. Not terribly complex.
   console.log(`TEST 1: ${req.params.id}`);
   res.redirect("/urls");
 });
@@ -231,7 +244,10 @@ app.get("/urls/:id/edit", (req, res) => {
   if (idValid === false) {
     return res.send("<html><body>You are not the owner of this url!</body></html>");
   }
-  const id = req.params.id;
+  // all of the above are security mesures.
+
+  const id = req.params.id; // Don't remove this, the templateVars break if you do and I don't know why, but just don't.
+
   const templateVars = {
     id,
     longURL: urlDatabase[id]['longURL'],
@@ -244,7 +260,7 @@ app.get("/login", (req, res) => {
   if (req.session.user_id) {
     res.redirect('/urls');
   }
-  res.render("login");
+  res.render("login"); // Behold, yon common login page.
 });
 
 app.post("/login", (req, res) => {
@@ -252,13 +268,12 @@ app.post("/login", (req, res) => {
     urls: urlDatabase,
     user: users[req.session.user_id]
   };
-  // const hashedPassword = bcrypt.hashSync(req.body.password);
   if (!searchThroughUsers(req.body.email, 'email')) {
     return res.status(403).send(`No account was found using this email.`);
   }
   for (let object in users) {
     if (users[object]['email'] === req.body.email) {
-      if (bcrypt.compareSync(req.body.password, users[object].password)) {
+      if (bcrypt.compareSync(req.body.password, users[object].password)) { // this bit compares the passwords.
         req.session.user_id = users[object]['id'];
       } else {
         console.log(req.body.password,users[object].password);
@@ -270,11 +285,11 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  req.session.user_id = null;
+  req.session = null; // cookie clearing after you logout. 
   return res.redirect('/login');
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`); // a little note to the server runner.
 });
 
